e516e0622ac08ece7ac7b7554389f2d6f763e847
quandang08 <22211TT1369@mail.tdc.edu.vn>
2025-04-14 18:03:37 +0700

    Call method in TaskCard like UserList, Edit,..

--- src/components/tasks/TaskCard.jsx
+++ src/components/tasks/TaskCard.jsx
@@ -1,9 +1,61 @@
-import { IconButton } from "@mui/material";
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import React from "react";
+import { IconButton, Menu, MenuItem } from "@mui/material";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import React, { useState } from "react";
+import UserList from "../user/UserList";
+import SubmissionsList from "./SubmissionList";
+import EditTaskForm from "./EditTaskForm";
+
+const role = "ROLE_ADMIN";
 
 const TaskCard = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
   
+  
+
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleCloseUserList = () => {
+    setOpenUserList(false);
+  };
+
+  const [openUserList, setOpenUserList] = useState(false);
+  const handleOpenUserList = () => {
+    setOpenUserList(true);
+    handleMenuClose();
+  };
+
+  const [openSubmissionList, setOpenSubmissionList] = useState(false);
+  const handleCloseSubmissionList = () => {
+    setOpenSubmissionList(false);
+    handleMenuClose();
+  };
+
+  const handleOpenSubmissionList = () => {
+    setOpenSubmissionList(true);
+    handleMenuClose();
+  };
+
+  const [openUpdateTaskForm, setOpenUpdateTaskForm] = useState(false);
+  const handleCloseUpdateTaskForm = () => {
+    setOpenUpdateTaskForm(false);
+  }
+  const handleOpenUpdateTaskModel = () => {
+    setOpenUpdateTaskForm(true);
+    handleMenuClose();
+  };
+
+  const handleDeleteTask = () => {
+
+    handleMenuClose();
+  };
+
   return (
     <div className="">
       <div className="card lg:flex justify-between">
@@ -34,12 +86,52 @@ const TaskCard = () => {
           </div>
         </div>
 
-        <div className="">
-          <IconButton>
-            <MoreHorizIcon/>
+        <div>
+          <IconButton
+            id="demo-positioned-button"
+            aria-controls={open ? "demo-positioned-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleMenuClick}
+          >
+            <MoreHorizIcon />
           </IconButton>
+
+          <Menu
+            id="demo-positioned-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleMenuClose}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+          >
+            {role === "ROLE_ADMIN" ? (
+              <>
+                <MenuItem onClick={handleOpenUserList}>Assigned User</MenuItem>
+                <MenuItem onClick={handleOpenSubmissionList}>
+                  See Submissions
+                </MenuItem>
+                <MenuItem onClick={handleOpenUpdateTaskModel}>Edit</MenuItem>
+                <MenuItem onClick={handleDeleteTask}>Delete</MenuItem>
+              </>
+            ) : (
+              <></>
+            )}
+          </Menu>
         </div>
       </div>
+      <UserList open={openUserList} handleClose={handleCloseUserList} />
+      <SubmissionsList
+        open={openSubmissionList}
+        handleClose={handleCloseSubmissionList}
+      />
+      <EditTaskForm open={openUpdateTaskForm} handleClose={handleCloseUpdateTaskForm} />
     </div>
   );
 };
