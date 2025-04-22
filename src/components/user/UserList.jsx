6846cfe6364c1f8cc0751e9c7bb57e46020393e5
quandang08 <22211TT1369@mail.tdc.edu.vn>
2025-04-22 10:27:39 +0700

    feat: Loading user data from database to select user do task

--- src/components/user/UserList.jsx
+++ src/components/user/UserList.jsx
@@ -10,6 +10,8 @@ import {
 } from "@mui/material";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserList } from "../../features/auth/AuthThunk";
 
 const style = {
   position: "absolute",
@@ -23,9 +25,15 @@ const style = {
   p: 4,
 };
 
-const users = [1, 1, 1, 1];
-
 export default function UserList({ handleClose, open }) {
+  const dispatch = useDispatch();
+  const { auth } = useSelector((store) => store);
+
+  // Fetch the user list when the component mounts
+  React.useEffect(() => {
+    dispatch(getUserList(localStorage.getItem("jwt")));
+  }, [dispatch]);
+
   return (
     <Modal
       open={open}
@@ -35,71 +43,84 @@ export default function UserList({ handleClose, open }) {
     >
       <Box sx={style}>
         <List>
-          {users.map((_, index) => (
-            <React.Fragment key={index}>
-              <ListItem
-                secondaryAction={
-                  <Button
-                    variant="outlined"
-                    onClick={handleClose}
-                    sx={{
-                      border: "2px solid #000",
-                      color: "#000",
-                      textTransform: "uppercase",
-                      fontWeight: "bold",
-                      padding: "0.5rem 1rem",
-                      minWidth: "90px",
-                      borderRadius: 0,
-                      "&:hover": {
-                        backgroundColor: "#000",
-                        color: "#fff",
-                      },
-                    }}
-                  >
-                    Select
-                  </Button>
-                }
-                disableGutters
-                sx={{
-                  paddingY: "0.5rem",
-                  borderRadius: 0,
-                }}
-              >
-                <ListItemAvatar>
-                  <Avatar
-                    sx={{
-                      width: 40,
-                      height: 40,
-                      border: "2px solid #000",
-                      boxShadow: "4px 4px 0 0 #000",
-                      borderRadius: 0,
-                    }}
-                  />
-                </ListItemAvatar>
-                <ListItemText
-                  primary={
-                    <span
-                      style={{
-                        fontWeight: "bold",
+          {/* Check if auth.users is not empty */}
+          {auth.users && auth.users.length > 0 ? (
+            auth.users.map((user, index) => (
+              <React.Fragment key={index}>
+                <ListItem
+                  secondaryAction={
+                    <Button
+                      variant="outlined"
+                      onClick={handleClose}
+                      sx={{
+                        border: "2px solid #000",
+                        color: "#000",
                         textTransform: "uppercase",
-                        borderBottom: "2px solid #000",
+                        fontWeight: "bold",
+                        padding: "0.5rem 1rem",
+                        minWidth: "90px",
+                        borderRadius: 0,
+                        "&:hover": {
+                          backgroundColor: "#000",
+                          color: "#fff",
+                        },
                       }}
                     >
-                      Code with Amu
-                    </span>
+                      Select
+                    </Button>
                   }
-                  secondary={
-                    <span style={{ color: "#555", fontFamily: "monospace" }}>
-                      @code_with_amu
-                    </span>
-                  }
-                />
-              </ListItem>
-              {index !== users.length - 1 && (
-                <Divider sx={{ borderColor: "#000" }} />
-              )}
-            </React.Fragment>
-          ))}
+                  disableGutters
+                  sx={{
+                    paddingY: "0.5rem",
+                    borderRadius: 0,
+                  }}
+                >
+                  <ListItemAvatar>
+                    <Avatar
+                      sx={{
+                        width: 40,
+                        height: 40,
+                        border: "2px solid #000",
+                        boxShadow: "4px 4px 0 0 #000",
+                        borderRadius: 0,
+                      }}
+                    />
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={
+                      <span
+                        style={{
+                          fontWeight: "bold",
+                          textTransform: "uppercase",
+                          borderBottom: "2px solid #000",
+                        }}
+                      >
+                        {user.fullName || "Unknown User"}
+                      </span>
+                    }
+                    secondary={
+                      <span style={{ color: "#555", fontFamily: "monospace" }}>
+                        @
+                        {user.fullName
+                          ? user.fullName.split(" ").join("_").toLowerCase()
+                          : "unknown_user"}{" "}
+                      </span>
+                    }
+                  />
+                </ListItem>
+                {index !== auth.users.length - 1 && (
+                  <Divider sx={{ borderColor: "#000" }} /> 
+                )}
+              </React.Fragment>
+            ))
+          ) : (
+            <ListItem>
+              <ListItemText
+                primary="No users available"
+                secondary="Please check your connection or data."
+              />
+            </ListItem>
+          )}
         </List>
       </Box>
     </Modal>
